19d227c4e4f258173aa20ee7a2ac6324b9adbb3c
Ali_jan <hassanfarid667@gmail.com>
2024-11-12 01:13:10 +0500

    Day 44 done

---
+++ 44 day challenge/d44.ts
@@ -0,0 +1,57 @@
+/*Question 130:** Explain how to export a function from one JavaScript file and import it into another file.
+
+**Explain & TIP:** In modern JavaScript development, modules 
+allow you to divide your code into separate files. This makes your code more organized and maintainable. You can export functions, objects, or primitives from one file and import them into another.
+*/
+
+
+import  {add} from "./mathExport.js"
+// console.log(add(52,5));
+
+// In another file where you want to use the add function:
+// console.log(add(2, 3)); // Outputs: 5
+// Demonstrates importing the add function from mathFunctions.ts and using it.
+
+
+/*********************************************************************************** */
+
+/*Question 131:** Create two modules; one that exports a class, and another that imports the class and 
+  creates an instance.
+
+**Explain & TIP:** Classes can be modularized in the same way as functions. This encapsulation and reusability
+ is a cornerstone of modern JavaScript application structure.*/
+
+
+
+import { Person } from "./person.js";
+
+
+let person1 = new Person("Ahmed");       // Outputs: Hello, my name is Ahmed
+person1.greet();
+// This snippet imports the Person class and uses it to create an instance.
+
+
+
+
+/*Question 132:** Discuss the difference between default and named exports in JavaScript modules.
+
+**Explain & TIP:** JavaScript provides two main types of exports: default and named. A module can have only
+one default export, but multiple named exports. This flexibility supports various use cases in module design.
+*/
+
+// Importing named exports:
+import { utilOne, utilTwo } from "./utils.js";
+
+// Importing a default export:
+import Calculator from "./calculator.js";
+
+// This code illustrates the syntax and usage differences between default and named exports.
+
+
+
+
+
+
+
+
+
